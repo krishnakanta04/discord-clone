26ad7477e6e2333b415e518bc312e6d04057a230
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2023-12-16 13:15:28 +0530

    Added delete and leave server functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,7 +5,9 @@ import { CreateServerModal } from "../modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
-import { CreateChannelModal } from "../modals/create-channel-modal copy";
+import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -25,6 +27,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
